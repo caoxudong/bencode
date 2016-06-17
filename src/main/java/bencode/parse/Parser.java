6e36df46c9d7b18b5f0c396ba07a7cedb206a72f
caoxudong <xdcao@urowks.cc>
2016-06-17 19:36:07 +0800

    add parser class

---
+++ src/main/java/bencode/parse/Parser.java
@@ -0,0 +1,24 @@
+package bencode.parse;
+
+import bencode.type.BType;
+
+/**
+ * <p>解析器，将一段字节数组转换为B编码的数据。
+ *
+ * @author caoxudong
+ * @since 0.1.0
+ */
+public class Parser {
+
+  /**
+   * 
+   * @param content     待解析的内容
+   * @param offset      偏移量，从该偏移量开始解析
+   * @return            解析出的数据
+   * @since 0.1.0
+   */
+  public BType parseNext(byte[] content, int offset) {
+    return null;
+  }
+
+}
